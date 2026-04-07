64c6c8078e0407485fc19e2b9c21f99d8a7dee44
fccview <comm.campione@gmail.com>
2026-04-07 12:10:12 +0100

    fix tests

--- tests/unit/logger.test.ts
+++ tests/unit/logger.test.ts
@@ -2,22 +2,37 @@ import { afterEach, beforeEach, describe, expect, test } from "bun:test";
 import { logger } from "../../src/server/utils/logger";
 
 describe("logger", () => {
-  const orig = process.env.LOGGER;
+  const orig = process.env.LOG_LEVEL;
 
   beforeEach(() => {
-    delete process.env.LOGGER;
+    delete process.env.LOG_LEVEL;
   });
 
   afterEach(() => {
-    if (orig !== undefined) process.env.LOGGER = orig;
+    if (orig !== undefined) process.env.LOG_LEVEL = orig;
   });
 
-  test("debug does not throw when LOGGER is not set", () => {
+  test("debug does not throw when LOG_LEVEL is not set", () => {
     expect(() => logger.debug("ctx", "msg")).not.toThrow();
   });
 
-  test("debug does not throw when LOGGER=debug", () => {
-    process.env.LOGGER = "debug";
+  test("debug does not throw when LOG_LEVEL=debug", () => {
+    process.env.LOG_LEVEL = "debug";
+    expect(() => logger.debug("ctx", "msg")).not.toThrow();
+  });
+
+  test("debug does not throw when LOG_LEVEL=warning", () => {
+    process.env.LOG_LEVEL = "warning";
+    expect(() => logger.debug("ctx", "msg")).not.toThrow();
+  });
+
+  test("debug does not throw when LOG_LEVEL=error", () => {
+    process.env.LOG_LEVEL = "error";
+    expect(() => logger.debug("ctx", "msg")).not.toThrow();
+  });
+
+  test("debug does not throw when LOG_LEVEL=fatal", () => {
+    process.env.LOG_LEVEL = "fatal";
     expect(() => logger.debug("ctx", "msg")).not.toThrow();
   });
 
